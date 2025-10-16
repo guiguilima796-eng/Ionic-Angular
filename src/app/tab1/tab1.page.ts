f93d35fea7ca886155b1d5733845764db65db56f
Guilherme <guiguilima796@gmail.com>
2025-10-16 19:02:35 +0000

    adicionando imagens

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,13 +11,44 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
 export class Tab1Page {
   imagem: string | undefined;
   titulo: string | undefined;
-
+  idImagem: number = 0;
+  arrayImgs: string[] = ["assets/fla.jpg","assets/fla2.jpg","assets/fla3.jpg","assets/urubuFla1.png","assets/urubuFla2.png","assets/urubuFla3.png","assets/urubuFla4.png"];
+  
   mensagem: string = 'olá,Mundo!';
 
   constructor() {}
 
   changeBook(){
-    this.titulo = "Flamengo";
-    this.imagem = "assets/fla.jpg"
+    this.idImagem = this.idImagem + 1; 
+    
+
+    if (this.idImagem == 1) {
+      this.titulo = "Flamengo";
+      this.imagem = this.arrayImgs[0];
+    }else if(this.idImagem == 2){
+      this.titulo = "JJ";
+      this.imagem = this.arrayImgs[1];
+    }else if(this.idImagem == 3){
+      this.titulo = "Flamengo";
+      this.imagem = this.arrayImgs[2];
+    }else if(this.idImagem == 4){
+      this.titulo = "UrubuFla";
+      this.imagem = this.arrayImgs[3];
+    }else if(this.idImagem == 5){
+      this.titulo = "UrubuFla";
+      this.imagem = this.arrayImgs[4];
+    }else if(this.idImagem == 6){
+      this.titulo = "UrubuFla";
+      this.imagem = this.arrayImgs[5];
+    }else if(this.idImagem == 7){
+      this.titulo = "UrubuFla";
+      this.imagem = this.arrayImgs[6];
+    }else if(this.idImagem == 8){
+      this.idImagem = 1;
+      this.titulo = "Flamengo";
+      this.imagem = this.arrayImgs[0];
+    }
+    
+
   }
 }
