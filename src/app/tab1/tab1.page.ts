233d0310eb1c9f9c80f6e0bf3aaca2db34e2e47b
Guilherme <guiguilima796@gmail.com>
2025-10-23 18:20:28 +0000

    automatizando tarefa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,54 +1,67 @@
-import { Component } from '@angular/core';
-import { IonImg,IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,IonButton,IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IonImg, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonButton, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonImg,IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,IonButton,IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonImg, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit, OnDestroy {
   imagem: string | undefined;
   titulo: string | undefined;
   idImagem: number = 0;
-  arrayImgs: string[] = ["assets/fla.jpg","assets/fla2.jpg","assets/fla3.jpg","assets/urubuFla1.png","assets/urubuFla2.png","assets/urubuFla3.png","assets/urubuFla4.png"];
-  
-  mensagem: string = 'olá,Mundo!';
+  arrayImgs: string[] = ["assets/fla.jpg", "assets/fla2.jpg", "assets/fla3.jpg", "assets/urubuFla1.png", "assets/urubuFla2.png", "assets/urubuFla3.png", "assets/urubuFla4.png"];
+  mensagem: string = 'Olá, Mundo!';
+  intervalo: any;
+  progresso: number = 0;
+  tempoTrocaImagem: number = 3000; // 3 segundos para cada troca
 
   constructor() {}
 
-  changeBook(){
-    this.idImagem = this.idImagem + 1; 
-    
+  ngOnInit() {
+    this.iniciarTemporizador();
+  }
 
-    if (this.idImagem == 1) {
-      this.titulo = "Flamengo";
-      this.imagem = this.arrayImgs[0];
-    }else if(this.idImagem == 2){
-      this.titulo = "JJ";
-      this.imagem = this.arrayImgs[1];
-    }else if(this.idImagem == 3){
-      this.titulo = "Flamengo";
-      this.imagem = this.arrayImgs[2];
-    }else if(this.idImagem == 4){
-      this.titulo = "UrubuFla";
-      this.imagem = this.arrayImgs[3];
-    }else if(this.idImagem == 5){
-      this.titulo = "UrubuFla";
-      this.imagem = this.arrayImgs[4];
-    }else if(this.idImagem == 6){
-      this.titulo = "UrubuFla";
-      this.imagem = this.arrayImgs[5];
-    }else if(this.idImagem == 7){
-      this.titulo = "UrubuFla";
-      this.imagem = this.arrayImgs[6];
-    }else if(this.idImagem == 8){
-      this.idImagem = 1;
-      this.titulo = "Flamengo";
-      this.imagem = this.arrayImgs[0];
+  ngOnDestroy() {
+    if (this.intervalo) {
+      clearInterval(this.intervalo); // Limpa o temporizador quando o componente for destruído
     }
-    
+  }
 
+  iniciarTemporizador() {
+    this.atualizarImagem(); // Atualiza a imagem imediatamente ao iniciar
+
+    this.intervalo = setInterval(() => {
+      this.atualizarImagem();
+    }, this.tempoTrocaImagem);
+
+    this.atualizarBarraProgresso();
+  }
+
+  atualizarImagem() {
+    this.idImagem = (this.idImagem + 1) % this.arrayImgs.length; // Círculo: volta à primeira imagem após a última
+
+    this.titulo = `Imagem ${this.idImagem + 1}`;
+    this.imagem = this.arrayImgs[this.idImagem];
+  }
+
+  trocarImagem() {
+    this.atualizarImagem(); // Troca a imagem manualmente ao clicar
+  }
+
+  atualizarBarraProgresso() {
+    let tempoRestante = this.tempoTrocaImagem;
+    let progressoIntervalo = setInterval(() => {
+      if (tempoRestante <= 0) {
+        clearInterval(progressoIntervalo);
+        document.getElementById("progress-bar")!.style.width = "0%"; // Reset progress bar
+      } else {
+        let percentual = ((this.tempoTrocaImagem - tempoRestante) / this.tempoTrocaImagem) * 100;
+        document.getElementById("progress-bar")!.style.width = `${percentual}%`;
+        tempoRestante -= 100;
+      }
+    }, 100); // Atualiza a barra a cada 100ms
   }
 }
